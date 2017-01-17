2e75a6b70bdbc832d5ca6293da8ba43e15a53883
Keno Clayton <keno.clayton15@gmail.com>
2017-01-17 02:50:30 -0500

    Now auto-updates as you type

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,7 +2,15 @@ var quill = new Quill('#editor', {
     theme: 'snow'
 });
 
+var text = quill.container.firstChild.innerHTML;
+
+$(document).ready(function () {
+    quill.focus();
+});
+
 quill.on('selection-change', function(range, oldRange, source) {
+    text = quill.container.firstChild.innerHTML;
+    convertHTML(text);
     if (range) {
         $("#editorWindow").addClass("active");
         console.log("In editor")
@@ -12,28 +20,30 @@ quill.on('selection-change', function(range, oldRange, source) {
     }
 });
 
-function convertHTML(text) {
+quill.on('text-change', function(delta, oldDelta, source) {
+    text = quill.container.firstChild.innerHTML;
+    convertHTML(text);
+});
+
+function convertHTML(convertText) {
     //TODO: Add Strikethrough, Spoiler, No Parse, Quote, Code, Table
-    
     //Process the markup tags
-    var markupText = text.replace(/<p>/g, '').replace(/<\/p>/g, '\n').replace(/<strong>/g, '[b]').replace(/<\/strong>/g, '[/b]').replace(/<h1>/g, '[h1]').replace(/<\/h1>/g, '[/h1]\n').replace(/<em>/g, '[i]').replace(/<\/em>/g, '[/i]').replace(/<u>/g, '[u]').replace(/<\/u>/g, '[/u]').replace(/<ol>/g, '[olist]\n').replace(/<\/ol>/g, '[/olist]\n').replace(/<ul>/g, '[list]\n').replace(/<\/ul>/g, '[/list]\n').replace(/<li>/g, '[*]').replace(/<\/li>/g, '\n').replace(/<br>/g, '\n').replace(/<a href="/g, '[url=').replace(/" target="_blank">/g ,']').replace(/<\/a>/g ,'[/url]');
+    var markupText = convertText.replace(/<p>/g, '').replace(/<\/p>/g, '\n').replace(/<strong>/g, '[b]').replace(/<\/strong>/g, '[/b]').replace(/<h1>/g, '[h1]').replace(/<\/h1>/g, '[/h1]\n').replace(/<em>/g, '[i]').replace(/<\/em>/g, '[/i]').replace(/<u>/g, '[u]').replace(/<\/u>/g, '[/u]').replace(/<ol>/g, '[olist]\n').replace(/<\/ol>/g, '[/olist]\n').replace(/<ul>/g, '[list]\n').replace(/<\/ul>/g, '[/list]\n').replace(/<li>/g, '[*]').replace(/<\/li>/g, '\n').replace(/<br>/g, '\n').replace(/<a href="/g, '[url=').replace(/" target="_blank">/g ,']').replace(/<\/a>/g ,'[/url]');
 
     //Process the preview
     //var previewText = text;
 
     //Set the markup display
-    $('#markup').html("<h2 class=\"lead\">Review Markup (Copy and Paste this to your review)</h2><br>" + markupText);
+    $('#markup').html(markupText);
     
     //Set the preview display
-    $('#preview').html("<h2 class=\"lead\">Review Preview</h2><br>" + text);
+    $('#preview').html(convertText);
 }
 
 //Show Preview
 $('#previewCollapse').on('show.bs.collapse', function() {
     quill.blur();
-    var text = quill.container.firstChild.innerHTML;
     console.log(text);
-    convertHTML(text);
     document.getElementById("showBtn").innerHTML = "Hide Preview";
 });
 
